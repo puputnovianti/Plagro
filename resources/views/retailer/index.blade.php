9fd9e2e32c021e2a4bbf15bb02a65eeed40c48f2
puputnovianti <puputnovianti99@gmail.com>
2022-02-06 14:26:43 +0800

    mengubah halaman index retailer

--- resources/views/retailer/index.blade.php
+++ resources/views/retailer/index.blade.php
@@ -1,4 +1,4 @@
-hallo retailer
+selamat datang retailer
 <form action="/logout" method="post">
     @csrf
     <button type="submit" class="nav-link px-3 bg-dark border-0">Logout <span data-feather="log-out"></span></button>
