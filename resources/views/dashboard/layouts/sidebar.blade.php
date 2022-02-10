4b17860b213c0a7bd3e718432120bb89f9c32302
puputnovianti <puputnovianti99@gmail.com>
2022-02-10 23:14:30 +0800

    mengubah typo di sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
       <li class="nav-item">
         <a class="nav-link {{ Request::is('dashboard/criterias*') ? 'active' : '' }}" href="/dashboard/criterias">
           <span data-feather="list"></span>
-          Kriterira
+          Kriteria
         </a>
       </li>
       <li class="nav-item">
